d16a2ba00e37f99276fa7399e245339c29bce373
OphisXSQDL <extremerezende@gmail.com>
2026-07-31 22:23:42 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int x = 10;
+        int y = 10;
+
+        System.out.println("O resultado é: "+ (x+y));
+    }
+}
